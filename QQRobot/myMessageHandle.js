7c7a9213456102ab0df65ce91534773e82b4304e
Gergul <mail0668@gmail.com>
2017-06-08 17:40:42 +0800

    Add files via upload

--- QQRobot/myMessageHandle.js
+++ QQRobot/myMessageHandle.js
@@ -39,10 +39,17 @@ function tulingReply(bot, msg)
 var localStorage = {};
 if(window.localStorage)
 	localStorage = window.localStorage;
+//是否开启图灵回复
 if (localStorage['tuling_running'] == undefined || 
 	localStorage['tuling_running'] == null)
 {
 	localStorage['tuling_running'] = "1";
+}
+//是否开启群回复
+if (localStorage['tuling_reply_group'] == undefined || 
+	localStorage['tuling_reply_group'] == null)
+{
+	localStorage['tuling_reply_group'] = "1";
 }
 
 //轮询消息句柄
@@ -51,21 +58,49 @@ function myMessageHandle(bot, msg)
 	var targetInfo = bot.getCurrentChatTargetInfo();
 	console.info(targetInfo['title']);
 	
-	if (msg['message'] == "robot:stop")
+	var cmd = msg['message'].toLowerCase();
+	if (cmd == "robot:help")
+	{
+		bot.sendMessage(
+		"机器人命令\n帮助：robot:help\n开启：robot:start\n关闭：robot:stop\n开启群：robot:startgroup\n关闭群：robot:stopgroup\n\n机器人源码：https://github.com/gergul/QQRobotChromeExtension"
+		);
+		return ;
+	}
+	else if (cmd == "robot:stop")
 	{
 		localStorage['tuling_running'] = "0";
 		bot.sendMessage("Robot:stopped");
 		return ;
 	}
-	else if (msg['message'] == "robot:start")
+	else if (cmd == "robot:start")
 	{
 		localStorage['tuling_running'] = "1";
 		bot.sendMessage("Robot:started");
 		return ;
 	}
+	else if (cmd == "robot:stopgroup")
+	{
+		localStorage['tuling_reply_group'] = "0";
+		bot.sendMessage("Robot:group stopped");
+		return ;
+	}
+	else if (cmd == "robot:startgroup")
+	{
+		localStorage['tuling_reply_group'] = "1";
+		bot.sendMessage("Robot:group started");
+		return ;
+	}
 	
-	if (localStorage['tuling_running']=="1" && msg['message'].indexOf("Robot:") < 0)
-		tulingReply(bot, msg);
+	//不进行消息处理
+	if (localStorage['tuling_running'] == "0" || 
+		(msg['group'] == 1 && localStorage['tuling_reply_group'] == "0") ||
+		msg['message'].toLowerCase().indexOf("robot:") == 0 )
+	{
+		return ;
+	}
+
+	//进行图灵回复
+	tulingReply(bot, msg);
 }
 
 //注册
