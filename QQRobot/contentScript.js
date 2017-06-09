f11d37b1c89788b1a36f6540d605b606abbacd8f
Gergul <mail0668@gmail.com>
2017-06-09 18:44:05 +0800

    Add files via upload

--- QQRobot/contentScript.js
+++ QQRobot/contentScript.js
@@ -13,6 +13,10 @@ var include = function(path){
 	return false;
 };
 
+
+
+//class QQRobot
+var globalInstance = null;//在class中使用延时函数setTimeout setInterval需要用到这个全局实例
 function QQRobot()
 {
 	this._switch = true;
@@ -35,6 +39,13 @@ function QQRobot()
 	}
 
 
+	this.getMyName = function(){
+		var nameNode = document.getElementById("mainTopAll").getElementsByClassName('text_ellipsis user_nick');
+		if (nameNode.length > 0)
+			return nameNode[0].textContent;
+		return undefined;
+	}
+	
 	//获得当前弹出的消息框中的消息列表
 	this.getCurrentMessageList = function(){
 		var root = document.getElementById("panelBody-5");
@@ -119,107 +130,149 @@ function QQRobot()
 		document.getElementById('send_chat_btn').click();
 	}
 	
+	this.sendIndex = 0;
+	this.txt = "";
+	this.splitNum = 200;
+	this.sleep = 2000;
+	this.sendNext = function(){
+		globalInstance.lock();
+		
+		var sub = globalInstance.txt.substr(globalInstance.sendIndex, globalInstance.splitNum);
+		if (sub != "")
+		{
+			globalInstance.sendMessage(sub);
+			globalInstance.sendIndex += globalInstance.splitNum;
+			setTimeout(globalInstance.sendNext, globalInstance.sleep);
+		}
+		else
+		{
+			sub = globalInstance.txt.substr(globalInstance.sendIndex, globalInstance.txt.length-globalInstance.sendIndex);
+			if (sub != "")
+				globalInstance.sendMessage(sub);
+			
+			globalInstance.sendIndex = 0;
+			globalInstance.txt = "";
+			globalInstance.splitNum = 200;
+			globalInstance.sleep = 2000;
+			globalInstance.unLock();
+		}
+	};
+	
+	//发送长消息
+	this.sendLongMessageAndUnlockAfter = function(txt, splitNum, sleep){
+		this.splitNum = arguments[1] ? arguments[1] : 200;//设置参数splitNum的默认值
+		this.sleep = arguments[2] ? arguments[2] : 5000;//设置参数sleep的默认值
+		this.sendIndex = 0;
+		this.txt = txt;
+		
+		this.sendNext();
+	}
+	
 	this.messageHandle = function(bot, msg){
 		console.info(msg);
 	};
-}
 	
-//轮询消息
-var needRefresh = false;//需要刷新页面
-var lastMessageList = new Map();//历史纪录<"id", [lastList]>
-var indexChat = 0;
-var pollMessageHandle = 0;
-var startPollMessage = function(bot) {
-	pollMessageHandle = setInterval(function(){
-		if (!bot._switch)
-			return ;
-		if (bot.isLocked())
-			return ;
-		
-		var chatList = bot.getCurrentChatList();
-		if (!chatList[indexChat])
-			indexChat = 0;
-		
-		
-		if (chatList[indexChat])
-		{
-			do 
-			{
-				//console.info(chatList[indexChat]);
-				chatList[indexChat].click();
-				
-				//检查是否有新消息
-				var curList = bot.getCurrentMessageList();
-				var targetInfo = bot.getCurrentChatTargetInfo();
-				var lastList = lastMessageList.get(targetInfo['title']);
-				if (lastList == undefined)
-				{
-					lastList = [];
-				}
-				lastMessageList.put(targetInfo['title'], curList);	
-				var newCount = curList.length - lastList.length;
-				
-				//为了保证在当前有消息时不刷新页面，
-				//  检查第一个会话是否有新的消息，有的话暂时不刷新
-				if (0 == indexChat && //第1个会话
-				    0 == newCount &&  //没有新的消息
-					needRefresh)      //需要刷新
-				{
-					window.open(document.URL, "_self");
-					return ;
-				}
-				
-				if (0 == newCount)
-					break;//没有新消息
-				
-				for (var i=lastList.length; i<curList.length; ++i)
-				{
-					var msg = curList[i];
-					if (msg['type'] == 0)
-					{
-						bot.messageHandle(bot, msg);
-					}
-				}
-			} while(false);
+	//轮询消息
+	this.needRefresh = false;//需要刷新页面
+	this.lastMessageList = new Map();//历史纪录<"id", [lastList]>
+	this.indexChat = 0;
+	this.pollMessageHandle = 0;
+	this.startPollMessage = function() {
+		this.pollMessageHandle = setInterval(function(){
+			if (!globalInstance._switch)
+				return ;
+			if (globalInstance.isLocked())
+				return ;
 			
-			++indexChat;
-		}
-		else
-		{
-			if (needRefresh)//刷新
-				window.open(document.URL, "_self");
-		}
-	}, 500);
-};
+			var chatList = globalInstance.getCurrentChatList();
+			if (!chatList[globalInstance.indexChat])
+				globalInstance.indexChat = 0;
+			
+			
+			if (chatList[globalInstance.indexChat])
+			{
+				do 
+				{
+					//console.info(chatList[globalInstance.indexChat]);
+					chatList[globalInstance.indexChat].click();
+					
+					//检查是否有新消息
+					var curList = globalInstance.getCurrentMessageList();
+					var targetInfo = globalInstance.getCurrentChatTargetInfo();
+					var lastList = globalInstance.lastMessageList.get(targetInfo['title']);
+					if (lastList == undefined)
+					{
+						lastList = [];
+					}
+					globalInstance.lastMessageList.put(targetInfo['title'], curList);	
+					var newCount = curList.length - lastList.length;
+					
+					//为了保证在当前有消息时不刷新页面，
+					//  检查第一个会话是否有新的消息，有的话暂时不刷新
+					if (0 == globalInstance.indexChat && //第1个会话
+						0 == newCount &&  //没有新的消息
+						globalInstance.needRefresh)      //需要刷新
+					{
+						window.open(document.URL, "_self");
+						return ;
+					}
+					
+					if (0 == newCount)
+						break;//没有新消息
+					
+					for (var i=lastList.length; i<curList.length; ++i)
+					{
+						var msg = curList[i];
+						if (msg['type'] == 0)
+						{
+							globalInstance.messageHandle(globalInstance, msg);
+						}
+					}
+				} while(false);
+				
+				++globalInstance.indexChat;
+			}
+			else
+			{
+				if (globalInstance.needRefresh)//刷新
+					window.open(document.URL, "_self");
+			}
+		}, 500);
+	};
 
-//定时刷新页面，防止登录信息失效
-var refreshMinutes = 5;//几分钟刷新一次
-var refreshIndex = 0;
-var keepAliveHandle = 0;
-var startKeepAlive = function(bot) {
-	keepAliveHandle = setInterval(function(){
-		if (refreshIndex < refreshMinutes-1)
-		{
-			++refreshIndex;
-			return ;
-		}
-		else
-			refreshIndex = 0;
-		
-		if (!bot._switch)
-			return ;
-		
-		needRefresh = true;
-		//window.open(document.URL, "_self");
-	}, 60000);
-};
+	//定时刷新页面，防止登录信息失效
+	this.refreshMinutes = 5;//几分钟刷新一次
+	this.refreshIndex = 0;
+	this.keepAliveHandle = 0;
+	this.startKeepAlive = function() {
+		globalInstance.keepAliveHandle = setInterval(function(){
+			if (globalInstance.refreshIndex < globalInstance.refreshMinutes-1)
+			{
+				++globalInstance.refreshIndex;
+				return ;
+			}
+			else
+				globalInstance.refreshIndex = 0;
+			
+			if (!globalInstance._switch)
+				return ;
+			
+			globalInstance.needRefresh = true;
+			//window.open(document.URL, "_self");
+		}, 60000);
+	};
+
+}//end QQRobot class
+
+
 
 var qqBot = new QQRobot();
+globalInstance = qqBot;
 function main()
 {
 	console.log("contentscript injected!");
-	startPollMessage(qqBot);
-	startKeepAlive(qqBot);
+	qqBot.startPollMessage();
+	qqBot.startKeepAlive();
 	qqBot.messageHandle = registerMessageHandle();
 
 	chrome.runtime.onMessage.addListener(function(request, sender, senderResponse){
